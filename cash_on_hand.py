d656acb55905c17360a8f68b12630cd21df1ec77
nlxrzn <S101262575@connect.np.edu.sg>
2023-08-08 00:01:01 +0800

    finalised coh

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -4,40 +4,38 @@ import csv
 # Create a file path to the CSV file
 fp = Path("csv_report/cash_on_hand.csv")
 
-#read the csv file to append day and ammount from the csv
-with fp.open(mode ="r", encoding = "UTF-8", newline = "") as file:
-     reader = csv.reader(file)
-     next(reader) #skip header
-
-     #create an empty lists to store day and amount record
-     coh=[]
-
-     #append cash on hand into cashrecords lis
-     for row in reader:
-        coh.append([int(row[0]), int(row[1])])
-
-def coh_differences(coh):
+def coh_differences(fp):
     """Computes the difference in Cash-on-Hand if the current day is lower than the previous day.
 
-    Args:
-    data: A list of lists, where each inner list contains the Day and Cash-on-Hand values for a single day.
+    1 parameter:
+    fp: A Path object pointing to the CSV file.
 
     Returns:
     A list of tuples, where each tuple contains the Day and Cash-on-Hand in Cash-on-Hand for a single day.
     """
     difference_in_cash_on_hand = []
-    for day in range(len(coh)):
-        if day == 0:
-            continue
-        previous_cash_on_hand = coh[day - 1][1]
-        cash_on_hand = coh[day][1]
+
+    #Open CSV file
+    with fp.open(mode ="r", encoding = "UTF-8", newline = "") as file:
+     reader = csv.reader(file)
+     next(reader) #skip header
+
+     # Initialize the previous_cash_on_hand
+     previous_cash_on_hand = 0
+    
+     for row in reader:
+        day = int(row[0])
+        cash_on_hand = int(row[1])
+            
+         
         if cash_on_hand < previous_cash_on_hand:
-            difference_in_cash_on_hand.append((coh[day][0], cash_on_hand))
-    for day, cash_on_hand in difference_in_cash_on_hand:
-        print(f"Day: {day}, Amount: {cash_on_hand}")
+            difference_in_cash_on_hand.append((day, cash_on_hand))
+           
+        previous_cash_on_hand = cash_on_hand # Update the previous_cash_on_hand for the next iteration  
+            
     return difference_in_cash_on_hand
 
-differences = coh_differences(coh)
+differences = coh_differences(fp)
 
     # Create a path object for the summary_report.txt in the home directory
 output_file_path = Path("summary_report.txt")
