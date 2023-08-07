929e8931403cbbbec3286f85bebecd42de38cd0b
nlxrzn <S101262575@connect.np.edu.sg>
2023-08-07 16:45:56 +0800

    updated main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import cash_on_hand
+import overheads, cash_on_hand
 
 def main():
-    #py name.function()
+    overheads.find_highest_overhead_category()
     cash_on_hand.coh_differences()
     #py nama.function()
 
