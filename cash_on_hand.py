61b9f56792d494dc76dcea513d1080b539c35d42
darisng <S10262525@connect.np.edu.sg>
2023-08-10 17:17:15 +0800

    Finalised

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,17 +1,12 @@
 from pathlib import Path
 import csv
 
-# Create a file path to the CSV file
-
 def coh_differences():
     """Computes the difference in Cash-on-Hand if the current day is lower than the previous day.
 
-
     Returns:
-    A list of tuples, where each tuple contains the Day and Cash-on-Hand in Cash-on-Hand for a single day.
+    A list of tuples, where each tuple contains the Day and Cash-on-Hand difference for a single day.
     """
-    difference_in_cash_on_hand = []
-
     difference_in_cash_on_hand = []
 
     # Create a file path to the CSV file
@@ -23,29 +18,26 @@ def coh_differences():
         next(reader)  # skip header
 
         # Initialize the previous_cash_on_hand
-        previous_cash_on_hand = 0
+        previous_cash_on_hand = 0.0  # Change this to a floating-point number
 
         for row in reader:
             day = int(row[0])
-            cash_on_hand = int(row[1])
+            cash_on_hand = int(row[1])  # Parse the cash on hand as a float
 
             if cash_on_hand < previous_cash_on_hand:
                 difference = previous_cash_on_hand - cash_on_hand
                 difference_in_cash_on_hand.append((day, difference))
-           
-        previous_cash_on_hand = cash_on_hand # Update the previous_cash_on_hand for the next iteration  
             
+            previous_cash_on_hand = cash_on_hand  # Move this line within the loop
+
     return difference_in_cash_on_hand
 
 cash_differences = coh_differences()
 
-    # Create a path object for the summary_report.txt in the home directory
+# Create a path object for the summary_report.txt in the home directory
 output_file_path = Path("summary_report.txt")
 
-    # Use mode="a" to append data to the file
+# Use mode="a" to append data to the file
 with output_file_path.open(mode="a", encoding="UTF-8") as output_file:
     for day, difference in cash_differences:
-        output_file.write(f"\n[CASH DEFICIT]Day: {day}, Amount: USD{difference}")
-
-
-
+        output_file.write(f"\n[CASH DEFICIT] Day: {day}, Amount: USD {difference}")  # Format the difference as a float with 2 decimal places
